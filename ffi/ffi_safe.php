6555fc9cd2ca329aa8b8a00c6391c00f958f4fcc
Xinchen Hui <laruence@gmail.com>
2020-03-12 13:44:16 +0800

    move constants to preload script

--- ffi/ffi_safe.php
+++ ffi/ffi_safe.php
@@ -1,11 +1,4 @@
 <?php
-const CURLE_OK = 0;
-const CURLOPT_URL = 10002;
-const CURLOPT_SSL_VERIFYHOST = 81;
-const CURLOPT_SSL_VERIFYPEER = 64;
-const CURLOPT_WRITEDATA = 10001;
-const CURLOPT_WRITEFUNCTION = 20011;
-
 $libcurl = get_libcurl();
 $write  =  get_write();
 $data = get_write_data($write);
@@ -15,10 +8,10 @@ $url = "https://www.laruence.com/2020/03/11/5475.html";
 
 $ch = $libcurl->curl_easy_init();
 
-$libcurl->curl_easy_setopt($ch, CURLOPT_URL, $url); 
-$libcurl->curl_easy_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$libcurl->curl_easy_setopt($ch, CURLOPT_WRITEDATA, get_data_addr($data));
-$libcurl->curl_easy_setopt($ch, CURLOPT_WRITEFUNCTION, $write->init());
+$libcurl->curl_easy_setopt($ch, CURLOPT::URL, $url); 
+$libcurl->curl_easy_setopt($ch, CURLOPT::SSL_VERIFYPEER, 0);
+$libcurl->curl_easy_setopt($ch, CURLOPT::WRITEDATA, get_data_addr($data));
+$libcurl->curl_easy_setopt($ch, CURLOPT::WRITEFUNCTION, $write->init());
 $libcurl->curl_easy_perform($ch);
 
 $libcurl->curl_easy_cleanup($ch);
