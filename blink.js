cff454ef0ca118cbe692714a6876a2c1f80e42a0
Xinchen Hui <laruence@gmail.com>
2020-05-23 22:52:35 +0800

    Use ansi color

--- blink.js
+++ blink.js
@@ -1,1 +1,26 @@
-t.prefs_.set('cursor-color', 'rgba(31,151,31,0.8)');
+black       = '#000000';
+red         = '#BB0000';
+green       = '#00BB00';
+yellow      = '#BB00BB';
+blue        = '#0000BB';
+magenta     = '#BB00BB';
+cyan        = '#00BBBB';
+white       = '#ffffff';
+
+lightBlack  = '#555555';
+lightRed    = '#FF5555';
+lightGreen  = '#55FF55';
+lightYellow = '#FFFF55';
+lightBlue   = '#5555FF';
+lightMagenta= '#FF55FF';
+lightCyan   = '#55FFFF';
+lightWhite  = '#E0E0E0';
+
+t.prefs_.set('color-palette-overrides',
+		[ black, red, green, yellow, blue, magenta, cyan, white,
+		lightBlack, lightRed, lightGreen, lightYellow,
+		lightBlue, lightMagenta, lightCyan, lightWhite]);
+
+t.prefs_.set('foreground-color', '#BBBBBB');
+t.prefs_.set('background-color', '#1B1B1B');
+t.prefs_.set('cursor-color', 'rgba(0,187,0,0.6)');
